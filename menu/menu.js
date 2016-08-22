c13c062823d1e0b8f9c3fa98cd9fb33d1f7624b1
André Pena <andrerpena@gmail.com>
2016-08-21 22:28:12 -0300

    Better organization. Bump to 1.3

---
+++ menu/menu.js
@@ -0,0 +1,307 @@
+window._menu = [
+    {
+        displayName: 'Contatos',
+        url: '/contatos',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/contatos',
+                menu: [
+                    {
+                        displayName: 'Alterar painel',
+                        url: '/config/widgets/edit/contatos?returnUrl=%2Fcontatos'
+                    }
+                ]
+            },
+            {
+                displayName: 'Contatos',
+                url: '/contatos/contatos/search'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/contatos/reportcontatos'
+            }
+        ]
+    },
+    {
+        displayName: 'Agenda',
+        url: '/agenda/compromissotarefa',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/agenda'
+            },
+            {
+                displayName: 'Compromissos e tarefas',
+                url: '/agenda/compromissotarefa'
+            },
+            {
+                displayName: 'Agenda do escritório',
+                url: '/agenda/AgendaEscritorio'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/agenda/reportagenda'
+            }
+        ]
+    },
+    {
+        displayName: 'Processos',
+        url: '/processos',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/processos'
+            },
+            {
+                displayName: 'Pastas',
+                url: '/processos/processos/search'
+            },
+            {
+                displayName: 'Casos',
+                url: '/processos/casos'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/processos/reportprocessos'
+            }
+        ]
+    },
+    {
+        displayName: 'Serviços',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/servicos'
+            },
+            {
+                displayName: 'Pastas',
+                url: '/servicos/servicos/search'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/servicos/reportservicos'
+            }
+        ]
+    },
+    {
+        displayName: 'Contratos',
+        url: '/contratos/contratocliente',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/contratos'
+            },
+            {
+                displayName: 'Contratos de cliente',
+                url: '/contratos/contratocliente'
+            },
+            {
+                displayName: 'Contratos de honorário',
+                url: '/contratos/contratos'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/contratos/reportcontratos'
+            }
+        ]
+    },
+    {
+        displayName: 'Financeiro',
+        url: '/financeiro/obrigacoes/Search',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/financeiro'
+            },
+            {
+                displayName: 'Contas a pagar e receber',
+                url: '/financeiro/obrigacoes/Search'
+            },
+            {
+                displayName: 'Fluxo de caixa',
+                url: '/financeiro/movimentos/search'
+            },
+            {
+                displayName: 'Inadimplência',
+                url: '/financeiro/Inadimplencias/search'
+            },
+            {
+                displayName: 'Solicitações',
+                url: '/financeiro/Solicitacoes/search'
+            },
+            {
+                displayName: 'Conciliação',
+                url: '/financeiro/Conciliacao/search'
+            },
+            {
+                displayName: 'Análises',
+                url: '/financeiro/analises'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/financeiro/ReportFinanceiro'
+            }
+        ]
+    },
+    {
+        displayName: 'Faturamento',
+        url: '/faturamento/itemfaturavel/search',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/faturamento/HomeFaturamento/Index'
+            },
+            {
+                displayName: 'Itens faturáveis',
+                url: '/faturamento/itemfaturavel/search'
+            },
+            {
+                displayName: 'Emissão',
+                url: '/faturamento/emissao/search'
+            },
+            {
+                displayName: 'Emitidas',
+                url: '/faturamento/emitida/search'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/faturamento/ReportFaturamento/index'
+            }
+        ]
+    },
+    {
+        displayName: 'Boletos',
+        url: '/boletos',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/boletos/HomeBoleto/Index'
+            },
+            {
+                displayName: 'Emissão',
+                url: '/boletos'
+            },
+            {
+                displayName: 'Emitidos',
+                url: '/boletos/Emitidos'
+            },
+            {
+                displayName: 'Conciliação',
+                url: '/boletos/ConciliacaoBoleto'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/boletos/ReportBoleto/index'
+            }
+        ]
+    },
+    {
+        displayName: 'GED',
+        url: '/GED/HomeGED',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/GED/HomeGED'
+            },
+            {
+                displayName: 'Arquivos',
+                url: '/GED/GEDArquivos/Search'
+            },
+            {
+                displayName: 'Gerador de documentos',
+                url: '/GED/GeradorDocumentos/Search'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/GED/ReportGED'
+            }
+        ]
+    },
+    {
+        displayName: 'Acervo jurídico',
+        url: '/acervojuridico/acervojuridico/search',
+        menu: [
+            {
+                displayName: 'Revista dos Tribunais Online',
+                url: '/acervojuridico/acervojuridico/search'
+            }
+        ]
+    },
+    {
+        displayName: 'Time Sheet',
+        url: '/TimeSheet/HorasTrabalhadas/Search',
+        menu: [
+            {
+                displayName: 'Painel',
+                url: '/TimeSheet/HomeTimeSheet'
+            },
+            {
+                displayName: 'Horas trabalhadas',
+                url: '/TimeSheet/HorasTrabalhadas/Search'
+            },
+            {
+                displayName: 'Análises',
+                url: '/TimeSheet/analises'
+            },
+            {
+                displayName: 'Relatórios',
+                url: '/TimeSheet/ReportTimeSheet'
+            }
+        ]
+    },
+    {
+        displayName: 'Workflow',
+        url: 'https://workflow.novajus.com.br/'
+    },
+    {
+        displayName: 'Mesas de trabalho',
+        url: '/MesasTrabalho'
+    },
+    {
+        displayName: 'Opções',
+        url: '/config/ferramentas',
+        menu: [
+            {
+                displayName: 'Ferramentas',
+                url: '/config/ferramentas'
+            },
+            {
+                displayName: 'Configurações',
+                url: '/config/configuracoes'
+            },
+            {
+                displayName: 'Tabelas de sistema',
+                url: '/config/TabelasSistema'
+            },
+            {
+                displayName: 'Controle de acesso',
+                url: '/config/CA'
+            },
+            {
+                displayName: 'Usuários',
+                url: '/config/usuarios'
+            },
+            {
+                displayName: 'Escritórios associados',
+                url: '/config/EscritorioAssociado'
+            },
+            {
+                displayName: 'Personalizações',
+                url: '/Personalizacoes/Modulos/Search'
+            },
+            {
+                displayName: 'Listas em Excel',
+                url: '/config/reportconfig/search'
+            },
+            {
+                displayName: 'Manuais',
+                url: '/config/Manuais'
+            },
+            {
+                displayName: 'Sobre',
+                url: '/config/Sobre'
+            }
+        ]
+    }
+];
