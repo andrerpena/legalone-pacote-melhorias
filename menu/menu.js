915fc71600c61965860ae97f720143c2b6eb103a
André Pena <andrerpena@gmail.com>
2016-08-23 19:17:41 -0300

    Mais menus

--- menu/menu.js
+++ menu/menu.js
@@ -19,12 +19,63 @@ window._menu = [
             {
                 displayName: 'Contatos',
                 url: '/contatos/contatos/search',
-                icon: 'user'
+                icon: 'user',
+                menu: [
+                    {
+                        displayName: 'Nova pessoa física',
+                        icon: 'plus-circle',
+                        url: '/contatos/pessoas/create'
+                    },
+                    {
+                        displayName: 'Nova pessoa jurídica',
+                        icon: 'plus-circle',
+                        url: '/contatos/empresas/create'
+                    },
+                    {
+                        displayName: 'Gerar relatório padrão',
+                        icon: 'bolt',
+                        url: '/contatos/ReportContatos/listacontatosgerar/53?ReportTitle=Contatos&ExportTo=0'
+                    },
+                    {
+                        displayName: 'Auditar contatos',
+                        icon: 'search',
+                        url: '/shared/auditoria/search?TipoObjeto=Novajus.Model.AGN_Pessoa%2CNovajus.Model.AGN_Empresa&titulo=Auditando%20pessoas%20f%C3%ADsicas%2Fjur%C3%ADdicas'
+                    }
+                ]
             },
             {
                 displayName: 'Relatórios',
                 url: '/contatos/reportcontatos',
-                icon: 'pie-chart'
+                icon: 'pie-chart',
+                menu: [
+                    {
+                        displayName: 'Relatórios gerados',
+                        icon: 'list',
+                        url: '/contatos/reportcontatos/Search',
+                        menu: [
+                            {
+                                displayName: 'Alterar painel',
+                                url: '/config/widgets/edit/contatos?returnUrl=%2Fcontatos',
+                                icon: 'pencil'
+                            }
+                        ]
+                    },
+                    {
+                        displayName: 'Novo modelo de relatório genérico de contato',
+                        icon: 'plus-circle',
+                        url: '/contatos/GenericReport?TipoAcaoModeloRelatorio=1'
+                    },
+                    {
+                        displayName: 'Novo modelo de relatório genérico de pessoa física',
+                        icon: 'plus-circle',
+                        url: '/contatos/PessoasGenericReport?TipoAcaoModeloRelatorio=1'
+                    },
+                    {
+                        displayName: 'Novo modelo de relatório genérico de pessoa jurídica',
+                        icon: 'plus-circle',
+                        url: '/contatos/EmpresasGenericReport?TipoAcaoModeloRelatorio=1'
+                    }
+                ]
             }
         ]
     },
@@ -37,11 +88,40 @@ window._menu = [
                 displayName: 'Painel',
                 url: '/agenda',
                 icon: 'bar-chart',
+                menu: [
+                    {
+                        displayName: 'Alterar painel',
+                        url: '/config/widgets/edit/agenda?returnUrl=%2Fagenda',
+                        icon: 'pencil'
+                    }
+                ]
             },
             {
                 displayName: 'Compromissos e tarefas',
                 url: '/agenda/compromissotarefa',
-                icon: 'calendar-o'
+                icon: 'calendar-o',
+                menu: [
+                    {
+                        displayName: 'Visualizar lista',
+                        url: '/agenda/compromissotarefa/search',
+                        icon: 'list'
+                    },
+                    {
+                        displayName: 'Novo compromisso',
+                        icon: 'plus-circle',
+                        url: '/agenda/compromissos/create'
+                    },
+                    {
+                        displayName: 'Nova tarefa',
+                        icon: 'plus-circle',
+                        url: '/agenda/tarefas/create'
+                    },
+                    {
+                        displayName: 'Auditar compromissos e tarefas',
+                        icon: 'search',
+                        url: '/shared/auditoria/search?TipoObjeto=Novajus.Model.AGN_CompromissoTarefa&titulo=Auditando%20compromissos%20e%20tarefas'
+                    }
+                ]
             },
             {
                 displayName: 'Agenda do escritório',
@@ -51,24 +131,80 @@ window._menu = [
             {
                 displayName: 'Relatórios',
                 url: '/agenda/reportagenda',
-                icon: 'pie-chart'
+                icon: 'pie-chart',
+                menu: [
+                    {
+                        displayName: 'Relatórios gerados',
+                        icon: 'list',
+                        url: '/agenda/reportagenda/Search'
+                    },
+                    {
+                        displayName: 'Novo modelo de relatório genérico',
+                        icon: 'plus-circle',
+                        url: 'href="/agenda/GenericReport?TipoAcaoModeloRelatorio=1"'
+                    },
+                    {
+                        displayName: 'Novo modelo de relatório genérico de compromisso',
+                        icon: 'plus-circle',
+                        url: '/agenda/CompromissoGenericReport?TipoAcaoModeloRelatorio=1'
+                    },
+                    {
+                        displayName: 'Novo modelo de relatório genérico de tarefa',
+                        icon: 'plus-circle',
+                        url: '/agenda/TarefaGenericReport?TipoAcaoModeloRelatorio=1'
+                    }
+                ]
             }
         ]
     },
     {
         displayName: 'Processos',
-        url: '/processos',
+        url: '/processos/processos/search',
         icon: 'legal',
         menu: [
             {
                 displayName: 'Painel',
                 url: '/processos',
                 icon: 'bar-chart',
+                menu: [
+                    {
+                        displayName: 'Alterar painel',
+                        url: '/config/widgets/edit/processos?returnUrl=%2Fprocessos',
+                        icon: 'pencil'
+                    }
+                ]
             },
             {
                 displayName: 'Pastas',
                 url: '/processos/processos/search',
-                icon: 'legal'
+                icon: 'legal',
+                menu: [
+                    {
+                        displayName: 'Novo processo',
+                        url: '/processos/processos/create',
+                        icon: 'plus-circle'
+                    },
+                    {
+                        displayName: 'Novo recurso',
+                        icon: 'plus-circle',
+                        url: '/processos/Recursos/create'
+                    },
+                    {
+                        displayName: 'Novo incidente',
+                        icon: 'plus-circle',
+                        url: '/processos/Incidentes/create'
+                    },
+                    {
+                        displayName: 'Gerar relatório padrão',
+                        icon: 'bolt',
+                        url: '/processos/ReportProcessos/listaprocessosgerar/44?ReportTitle=Processos&ExportTo=0'
+                    },
+                    {
+                        displayName: 'Auditar processos',
+                        icon: 'search',
+                        url: '/shared/auditoria/search?TipoObjeto=Novajus.Model.PRO_Processo&titulo=Auditando%20processos%2Frecursos%2Fincidentes'
+                    }
+                ]
             },
             {
                 displayName: 'Casos',
@@ -78,7 +214,19 @@ window._menu = [
             {
                 displayName: 'Relatórios',
                 url: '/processos/reportprocessos',
-                icon: 'pie-chart'
+                icon: 'pie-chart',
+                menu: [
+                    {
+                        displayName: 'Relatórios gerados',
+                        icon: 'list',
+                        url: '/processos/reportprocessos/Search'
+                    },
+                    {
+                        displayName: 'Novo modelo de relatório genérico',
+                        icon: 'plus-circle',
+                        url: '/processos/GenericReport?TipoAcaoModeloRelatorio=1'
+                    }
+                ]
             }
         ]
     },
